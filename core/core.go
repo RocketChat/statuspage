3c45898709dc33b444402c9f366cc87c972bf598
felipemenelau <felipe.menelau@rocket.chat>
2022-04-25 15:31:10 -0300

    Adjust project init and config to support regions

--- core/core.go
+++ core/core.go
@@ -9,7 +9,7 @@ import (
 
 var _dataStore store.Store
 
-//TwistItUp takes everything and starts the core up
+// TwistItUp takes everything and starts the core up
 func TwistItUp() error {
 	store, err := boltstore.New()
 	if err != nil {
@@ -18,15 +18,20 @@ func TwistItUp() error {
 
 	_dataStore = store
 
-	//Now that we have a store, let's ensure the services from the config exist
+	// Now that we have a store, let's ensure the services and regions from the config exist
 	if err := createServicesFromConfig(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Regions always need to be created AFTER services due to regions being tethered to services
+	if err := createRegionsFromConfig(); err != nil {
 		log.Fatalln(err)
 	}
 
 	return nil
 }
 
-//LivenessCheck checks the database to see if it responds to a ping
+// LivenessCheck checks the database to see if it responds to a ping
 func LivenessCheck() error {
 	return _dataStore.CheckDb()
 }
