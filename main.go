e1a9d8378059693d8e84501bdf2ba0b0c39ad646
felipemenelau <felipe.menelau@rocket.chat>
2022-04-25 15:32:57 -0300

    Adds swagger annotations and docs

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	"github.com/RocketChat/statuscentral/router"
 )
 
+// @title           Status Central
+// @version         0.1
+// @description    	Operational status for Rocket Chat SaaS & Cloud
+
+// @contact.name   Cloud Team
+// @contact.url    https://open.rocket.chat/group/cloud
+
+// @host status.rocket.chat
+// @BasePath  /api
 func main() {
 	configFile := flag.String("configFile", "statuscentral.yaml", "Config File full path. Defaults to current folder")
 
